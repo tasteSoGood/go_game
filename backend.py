37a570092c914eb535496fb60cf919572f621434
tasteSoGood <tastesogood@163.com>
2025-04-20 16:27:35 +0800

    加入更完整的规则：禁止自杀、劫的处理以及模拟提子过程

--- backend.py
+++ backend.py
@@ -18,87 +18,113 @@ class GoBoard:
         self.board = np.zeros((board_size, board_size), dtype=int)
         # self.board = np.random.randint(-1, 2, (board_size, board_size))
         self.current_player = "black"
+        # 处理“劫”规则
+        self.last_ko_position = None  # 劫的位置
+        self.last_ko_player = None    # 上次触发劫的玩家
 
-    def check_captured_stones(self):
-        """检查被提掉的棋子"""
-        self.qi_board = np.zeros_like(self.board, dtype=int) # 每个子的气
-        self.visited = np.zeros_like(self.board, dtype=bool) # 该棋子是否被访问过
-        # 需要等全部检查完之后才能改动棋盘
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.board[i, j] != 0 and not self.visited[i, j]:
-                    self.check_qi(i, j) # 把当前棋子的气记录下来
-
-        # 根据棋子的气来提子
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.qi_board[i, j] == 0:
-                    self.board[i, j] = 0
-
-        # print(self.qi_board)
-
-    def check_qi(self, i, j):
+    def cal_group_qi(self, x, y, temp_board):
         """
-        利用广度优先(BFS)检查 (x, y) 位置的棋子的气
-        由于围棋规则中相连的棋子具有相同的气，需要找到连通块
+        计算指定位置所在的连通块的气（基于一个临时的棋盘）
+        返回气的数量和所在的连通块
         """
-        # 找到连通块
-        color = self.board[i, j]
-        group = []
-        queue = deque([(i, j)])
-        self.visited[i, j] = True
-        
+        if temp_board[x, y] == 0: # 指定的位置没有棋子
+            return 0, []
+        color         = temp_board[x, y] # 指定位置的棋子类型
+        visited       = np.zeros_like(temp_board, dtype=bool) # 记录是否被访问过
+        queue         = deque([(x, y)])
+        visited[x, y] = True
+        group         = [] # 连通块
+        qi_set        = set() # 计算为气的空位置
+
         # 用BFS找到所有相连的同色棋子
         while queue:
-            x, y = queue.popleft()
-            group.append((x, y))
-
+            i, j = queue.popleft()
+            group.append((i, j))
             for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]: # 向四周搜索
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < self.size and 0 < ny < self.size:
-                    if self.board[nx, ny] == color and not self.visited[nx, ny]:
-                        self.visited[nx, ny] = True
-                        queue.append((nx, ny))
+                ni, nj = i + dx, j + dy
+                if 0 <= ni < self.size and 0 < nj < self.size:
+                    if temp_board[ni, nj] == 0:
+                        qi_set.add((ni, nj))
+                    elif temp_board[ni, nj] == color and not visited[ni, nj]:
+                        visited[ni, nj] = True
+                        queue.append((ni, nj))
+        return len(qi_set), group
 
-        # 计算该连通块的气
-        qi_set = set()
-        for x, y in group:
-            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]: # 向四周搜索
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < self.size and 0 < ny < self.size:
-                    if self.board[nx, ny] == 0:
-                        qi_set.add((nx, ny))
+    def find_captured_stones(self, temp_board):
+        """
+        返回所有敌方气为0的棋子
+        为了适应围棋规则，需要延后提子，先模拟计算落子后的气
+        """
+        captured = [] # 返回的位置列表
+        visited = np.zeros_like(temp_board, dtype=bool)
 
-        # 为连通块中的所有棋子分配相同的气
-        qi_count = len(qi_set)
-        for x, y in group:
-            self.qi_board[x, y] = qi_count
+        for i in range(self.size):
+            for j in range(self.size):
+                if temp_board[i, j] != 0 and not visited[i, j]:
+                    qi, group = self.cal_group_qi(i, j, temp_board)
+                    if qi == 0:
+                        captured.extend(group)
+                    for x, y in group:
+                        visited[x, y] = True
+
+        return captured
 
     def place_stone(self, x, y):
-        """落子"""
-
-        # TODO:
-        # 1. 当前落子之后，如果存在可以被提掉的对方子，则当前的落子成立
-        # 2. 当前落子之后，如果不存在可以被提掉的对方子，则当前位置不能落子
-
+        """
+        落子
+        需要满足高级的落子规则以及劫的规则
+        """
+        # 初步的检查
         if not (0 <= x < self.size and 0 <= y < self.size): # 越界
             return False
         if self.board[x, y] != 0: # 已经有子了
             return False
-        if self.current_player == "white":
-            self.board[x, y] = 1 # 用1表示落上白子
-            self.current_player = "black"
-            self.check_captured_stones() # 提子
-            return True
-        elif self.current_player == "black":
-            self.board[x, y] = -1 # 用0表示落上黑子
-            self.current_player = "white"
-            self.check_captured_stones() # 提子
-            return True
-        return False # 其他情况
+
+        # 劫争检查：禁止立即回提，至少需要间隔一手
+        if self.last_ko_position and (x, y) == self.last_ko_position:
+            if self.current_player != self.last_ko_player:
+                return False
+
+        # 创建一个临时棋盘用于模拟落子之后的状态
+        current_color = 1 if self.current_player == "white" else -1
+        temp_board = self.board.copy()
+        temp_board[x, y] = current_color
+
+        # 计算对方被提的棋子
+        enemy_color = -current_color
+        enemy_captured = [
+            (i, j) for i, j in self.find_captured_stones(temp_board)
+            if temp_board[i, j] == enemy_color
+        ]
+
+        # 模拟提掉对方的棋子
+        for i, j in enemy_captured:
+            temp_board[i, j] = 0
+
+        # 模拟提子后检查自己的气
+        my_qi, _ = self.cal_group_qi(x, y, temp_board)
+        if my_qi == 0 and len(enemy_captured) == 0:
+            return False # 禁止自杀行为（模拟结果：没有提掉对方的子，新落的子的气还为0）
+
+        # 模拟成功，更新棋盘
+        self.board = temp_board
+
+        # 记录劫的状态
+        if len(enemy_captured) == 1: # 只提掉对方一个子，此时构成了劫
+            self.last_ko_position = enemy_captured[0]
+            self.last_ko_player = self.current_player
+        else:
+            self.last_ko_position = None
+            self.last_ko_player = None
+
+        # 切换当前玩家
+        self.current_player = "white" if self.current_player == "black" else "black"
+        return True
 
     def reset(self):
         """ 重置 """
         self.board = np.zeros((self.size, self.size), dtype=int)
         self.current_player = "black"
+        self.last_ko_position = None
+        self.last_ko_player = None
 
